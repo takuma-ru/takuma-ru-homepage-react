8845f4acb6464b69d027b048d89d4703ce51ff9a
takuma-ru <kcattdev@gmail.com>
2022-05-05 00:38:11 +0900

    [Add] set up

---
+++ components/Frame.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import styles from '../styles/components/Frame.module.scss'
+
+const Frame: NextPage = () => {
+  return (
+    <div className={styles.frame}>
+    </div>
+  )
+}
+
+export default Frame
