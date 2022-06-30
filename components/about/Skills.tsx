7ed9e7cd83a0ebdaefeeef0bc90e33c84e7fc09a
takuma-ru <kcattdev@gmail.com>
2022-06-30 10:53:36 +0900

    [Update] Skills.tsc

--- components/about/Skills.tsx
+++ components/about/Skills.tsx
@@ -12,6 +12,7 @@ import Link from '../utils/Link';
 
 import colors from '../../styles/styledComponents/colors';
 import { mixins } from '../../styles/styledComponents/mixin'
+import dependsLuminanceColor from '../../styles/styledComponents/luminanceDependsColor';
 
 
 interface propsInterface {
@@ -37,6 +38,8 @@ interface skillDataInterface {
   skillLevel: 0 | 1 | 2 | 3 | 4 | 5,
   description?: ReactNode | string,
   chips?: Array<string>
+  row: string,
+  col: string,
 }
 
 const Skills: React.FC<propsInterface> = (props) => {
@@ -46,25 +49,33 @@ const Skills: React.FC<propsInterface> = (props) => {
       title: 'Vue.js',
       skillLevel: 5,
       description: '~ Vue3',
-      chips: ['framework']
+      chips: ['framework'],
+      row: '1/3',
+      col: '1/3',
     },
     {
       title: 'Nuxt.js',
       skillLevel: 5,
       description: '~ CompositionAPI',
-      chips: ['framework']
+      chips: ['framework'],
+      row: '3/5',
+      col: '1/3',
     },
     {
       title: 'React',
       skillLevel: 3,
       description: '~ CompositionAPI',
-      chips: ['framework']
+      chips: ['framework'],
+      row: '1/2',
+      col: '3/4',
     },
     {
       title: 'Flutter',
       skillLevel: 2,
       description: '~ CompositionAPI',
-      chips: ['framework']
+      chips: ['framework'],
+      row: '2/3',
+      col: '3/4',
     },
   ]
 
@@ -74,25 +85,32 @@ const Skills: React.FC<propsInterface> = (props) => {
 
   return <>
     <SkillsMain>
-      <div className='skill-card-group'>
+      <div className='skill-card-wrapper'>
         {skillData.map(skill => (
           <div
             key={skill.title}
             className='skill-card'
             style={{
-              width: `${
+              gridRow: skill.row,
+              gridColumn: skill.col,
+              /* width: `${
                 skill.skillLevel === 5 || skill.skillLevel === 4 || skill.skillLevel === 3
-                  ? 2 * 152
-                  : 152
+                  ? 2 * 128
+                  : 128
               }px`,
               height: `${
                 skill.skillLevel === 5 || skill.skillLevel === 4
-                  ? 2 * 152
-                  : 152
-              }px`,
+                  ? 2 * 128
+                  : 128
+              }px`, */
             }}
+            onClick={() => {}}
           >
-            {skill.title + skill.skillLevel}
+            <div className='skill-card-title'>
+              <h2>{ skill.title }</h2>
+              <Divider vertical />
+              <h4>{ skill.skillLevel }</h4>
+            </div>
           </div>
         ))}
       </div>
@@ -101,13 +119,9 @@ const Skills: React.FC<propsInterface> = (props) => {
 }
 
 const SkillsMain = styled.div`
-  display: grid;
-
   position: relative;
   width: 100%;
   height: 100%;
-
-  overflow: scroll;
 
   ${mixins.screenSm} {
     align-self: start;
@@ -119,8 +133,43 @@ const SkillsMain = styled.div`
     grid-template-columns: 128px 1fr;
   }
 
-  .skill-card {
-    background-color: ${colors('white')};
+  .skill-card-wrapper {
+    display: grid;
+    grid-auto-rows: 152px;
+    grid-auto-columns: 152px;
+
+    width: 100%;
+    height: 100%;
+
+    overflow: auto;
+
+    .skill-card {
+      padding: 16px;
+
+      background-color: ${colors('white')};
+      color: ${dependsLuminanceColor('white')};
+      cursor: pointer;
+
+      &:hover {
+        background-color: ${colors('black')};
+        color: ${dependsLuminanceColor('black')};
+      }
+
+      &-title {
+        display: flex;
+        align-items: center;
+
+        height: 32px;
+
+        h2 {
+          margin-right: 16px;
+        }
+
+        h4 {
+          margin-left: 16px;
+        }
+      }
+    }
   }
 `
 
